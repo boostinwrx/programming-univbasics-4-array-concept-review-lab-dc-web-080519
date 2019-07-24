a717d93e5088cfe97d1ca7aaf828bedb7befb77d
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-24 01:48:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,5 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  while counter < array.length do
-    array.index(array[counter])
-    counter += 1
-  end
+    array.index(value_to_find)
 end
 
 def find_max_value(array)
